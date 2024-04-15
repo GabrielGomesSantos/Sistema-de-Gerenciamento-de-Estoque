9bf3d66b321ae43a7641f083045d5bd9321b86a1
Gabriel Gomes <gabrielgones5@gmail.com>
2024-04-15 17:11:30 -0300

    sei la.php

--- src/pages/navbar_pesquisa.php
+++ src/pages/navbar_pesquisa.php
@@ -4,65 +4,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../assets/css/style_navbar.css">
-    <link rel="stylesheet" href="assets/css/style_navbar.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik+Scribble&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Rubik+Scribble&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 
 </head>
-<header>
-    
-    <div class="header">
-        <div class="img_logo">
-            <img src="assets/img/Chilling_codes.png" alt="">
-            <h1>Chilling Codes</h1>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg" style="background-color: #e4f4ff;">
+        <div class="container-fluid">
+            <div class="img_logo">
+                <img src="assets/img/Chilling_codes.png" alt="">
+            </div>
+
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <form class="d-flex" role="search" action="index.php" method="GET">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success " type="submit">Search</button>
+                </form>
+            </div>
+
+
+            <button type="button" class="btn btn-primary btn-lg" onclick="window.location='src/pages/sair.php'">Sair</button>
+
         </div>
+    </nav>
 
-        <div class="pesquisar">
-            <form class="pesquisar" action="index.php" method="GET">
-                <input class='inputext' type="text" name="busca" id="busca" placeholder="Digite sua busca aqui">
-            </form>
-        </div>    
-
-        
-
-    
-            <?php 
-                
-                if($_SESSION['perm'] == 1 or $_SESSION['perm'] == 3){
-                    echo("
-                        <div class='button_header'>
-                            <button onclick='window.location=\"src/pages/insert.php\"'>
-                                Adicionar
-                            </button>
-
-                            <button onclick='window.location=\"src/pages/sair.php\"'>
-                                Sair
-                            </button>
-                        </div>
-                    ");
-
-                }else{
-                    echo("
-                    <div class='button_header'>
-                        <button onclick='window.location=\"src/pages/sair.php\"'>
-                            Sair
-                        </button>
-                    </div>
-                ");
-                }
-            ?>
-             
-            
-        
-    </div>
-
-</header>
-
-</html>
+</body>
--- src/pages/read.php
+++ src/pages/read.php
@@ -19,86 +19,71 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/style_read.css">
     <link rel="stylesheet" href="assets/css/style_read.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Alata&family=Archivo+Black&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Rubik+Scribble&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
 
     <div class="content">
-        <table>
+            <table class="table table-dark table-striped">
 
-            <thead>
-    
-                <?php 
-                    if (mysqli_num_rows($result) > 0) { // Verifica se existem resultados
-                        if($_SESSION["perm"] != 2){
-                ?>
-                
-                <tr>
-                    <th>NOME</th>
-                    <th>DESCRIÇÃO</th>
-                    <th>QUANTIDADE</th>
-                    <th>PREÇO</th>
-                    <th colspan=2>AÇÃO</th>
-                </tr>
 
-                <?php }else{ ?>
-                
-                <tr>
-                    <th>NOME</th>
-                    <th>DESCRIÇÃO</th>
-                    <th>QUANTIDADE</th>
-                    <th>PREÇO</th>
-                </tr>
-                
-                <?php }
-                    } else {
-                ?>
+        <thead>
+            <?php 
+                if (mysqli_num_rows($result) > 0) { // Verifica se existem resultados
+                    if($_SESSION["perm"] != 2){
+            ?>
+            <tr>
+                <th>NOME</th>
+                <th>DESCRIÇÃO</th>
+                <th>QUANTIDADE</th>
+                <th>PREÇO</th>
+                <th colspan=2>AÇÃO</th>
+            </tr>
+            <?php }else{ ?>
+            <tr>
+                <th>NOME</th>
+                <th>DESCRIÇÃO</th>
+                <th>QUANTIDADE</th>
+                <th>PREÇO</th>
+            </tr>
+            <?php }
+                } else {
+            ?>
+            <div class="resultado">
+                <img src="assets/img/notificacao.svg" width="712px" alt="">
+                <br>
+                <p class="error_busca">NENHUM RESULTADO ENCONTRADO</p>
+            </div>
+            <?php } ?>
+        </thead>
 
-                <div class="resultado">
-                    <img src="assets/img/notificacao.svg" width="712px" alt="">
-                    <br>
-                    <p class="error_busca">NENHUM RESULTADO ENCONTRADO</p>
-                </div>
 
-                <?php } ?>
-                
-            </thead>
-            
-            <tbody> 
-                <?php 
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        if ($_SESSION["perm"] != 2) {
-                ?>
 
-                <tr id='<?php echo $row["id"]; ?>'>
-                    <td><?php echo $row["nome"]; ?></td>
-                    <td><?php echo $row["descricao"]; ?></td>
-                    <td><?php echo $row["qnt"]; ?></td>
-                    <td class='preco' >R$ <?php echo $row["preco"]; ?></td>
-                    <td> <a href='src/pages/update.php?id=<?php echo $row["id"]; ?>'><button class="button_acao">EDITAR</button></a></td>
-                    <td> <a href='src/pages/delete.php?id=<?php echo $row["id"]; ?>'><button class="button_acao">REMOVER</button></a></td>
-                </tr>
 
-                <?php } else { ?>
-
-                <tr id='<?php echo $row["id"]; ?>'>
-                    <td><?php echo $row["nome"]; ?></td>
-                    <td><?php echo $row["descricao"]; ?></td>
-                    <td><?php echo $row["qnt"]; ?></td>
-                    <td class='preco'>R$ <?php echo $row["preco"]; ?></td>
-                </tr>
-
-                <?php } } ?>
-            </tbody>
-        </table>    
+        <tbody> 
+            <?php 
+                while ($row = mysqli_fetch_assoc($result)) {
+                    if ($_SESSION["perm"] != 2) {
+            ?>
+            <tr id='<?php echo $row["id"]; ?>'>
+                <td><?php echo $row["nome"]; ?></td>
+                <td><?php echo $row["descricao"]; ?></td>
+                <td><?php echo $row["qnt"]; ?></td>
+                <td class='preco' >R$ <?php echo $row["preco"]; ?></td>
+                <td> <a href='src/pages/update.php?id=<?php echo $row["id"]; ?>'><button class="button_acao">EDITAR</button></a></td>
+                <td> <a href='src/pages/delete.php?id=<?php echo $row["id"]; ?>'><button class="button_acao">REMOVER</button></a></td>
+            </tr>
+            <?php } else { ?>
+            <tr id='<?php echo $row["id"]; ?>'>
+                <td><?php echo $row["nome"]; ?></td>
+                <td><?php echo $row["descricao"]; ?></td>
+                <td><?php echo $row["qnt"]; ?></td>
+                <td class='preco'>R$ <?php echo $row["preco"]; ?></td>
+            </tr>
+            <?php } } ?>
+        </tbody>
+        </table>
     </div> 
         
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
